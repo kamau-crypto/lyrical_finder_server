61f213297a3e62c3abe83b3059df6cd6691ab21e
Peter Kamau <kamaupeter343@gmail.com>
2024-02-01 00:31:33 +0300

    Added to the Song resolver module

--- src/song/song.module.ts
+++ src/song/song.module.ts
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { SongResolver } from './song.resolver';
 
 @Module({
-  providers: [SongResolver]
+  providers: [SongResolver],
 })
 export class SongModule {}
