c723021691c22fb2d66ae39ae70319c405d372f1
Peter Kamau <kamaupeter343@gmail.com>
2024-02-02 21:06:55 +0300

    Added the song-lyrics resolver

--- src/song-lyrics/song-lyrics.resolver.ts
+++ src/song-lyrics/song-lyrics.resolver.ts
@@ -1,4 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { Inject } from '@nestjs/common';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { MusicService } from 'src/musixmatch';
+import { TYPES } from 'src/musixmatch/ioc/types';
+import { SongLyricsEntity } from './song-lyrics.dto';
 
-@Resolver()
-export class SongLyricsResolver {}
+@Resolver('SongLyrics')
+export class SongLyricsResolver {
+  constructor(
+    @Inject(TYPES.MUSIC_SERVICE) private readonly musicService: MusicService,
+  ) {}
+  @Query(() => SongLyricsEntity)
+  songLyrics(@Args('id') id: string) {
+    return this.musicService.findLyrics(id);
+  }
+}
