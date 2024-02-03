a1f4153b59becef660e0c6060522da8b7a6f9ca8
Peter Kamau <kamaupeter343@gmail.com>
2024-02-03 09:03:09 +0300

    compiled the artist name resolver

--- src/artist/artist.resolver.ts
+++ src/artist/artist.resolver.ts
@@ -2,7 +2,7 @@ import { Inject } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { MusicServiceImpl } from 'src/musixmatch';
 import { MusicService } from '../musixmatch/services';
-import { TYPES } from 'src/musixmatch/ioc/types';
+import { TYPES } from '../musixmatch/ioc/types';
 import { ArtistEntity } from './artist.dto';
 
 @Resolver()
@@ -11,7 +11,7 @@ export class ArtistResolver {
     @Inject(TYPES.MUSIC_SERVICE)
     private readonly musicService: MusicService,
   ) {}
-  @Query()
+  @Query(() => [ArtistEntity])
   async artists(@Args('name') name: string): Promise<ArtistEntity[]> {
     return this.musicService.findArtists(name).toPromise();
   }
